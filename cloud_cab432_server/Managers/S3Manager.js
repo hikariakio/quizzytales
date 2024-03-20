ecc8eb71e74ee9bf01ed2e94bc7b56c7f1fbbf33
Ye Gaung Kyaw <31139816+hikariakio@users.noreply.github.com>
2024-03-20 23:43:22 +1000

    Delete SessionToken Field

--- cloud_cab432_server/Managers/S3Manager.js
+++ cloud_cab432_server/Managers/S3Manager.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  sessionToken: process.env.AWS_SESSION_TOKEN,
+  // sessionToken: process.env.AWS_SESSION_TOKEN,
   region: "ap-southeast-2",
 });
 
